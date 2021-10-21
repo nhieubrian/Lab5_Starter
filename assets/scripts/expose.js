6e1c7a872c90844fa4723c25cc8c17f273ba013f
nhieubrian <bnhieu@ucsd.edu>
2021-10-20 22:39:25 -0700

    first commit D:

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,20 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+var selectHorn;
+
 
 function init() {
-  // TODO
+  selectHorn = document.getElementById('horn-select')
+}
+
+selectHorn = document.getElementById('horn-select')
+if(selectHorn){
+  selectHorn.addEventListener('change', (event) => {
+
+    if(event.target.value == 'air-horn'){
+      selectHorn.setAttribute('src', "assets/images/air-horn.svg");
+    }
+    console.log(selectHorn);
+  });
 }
